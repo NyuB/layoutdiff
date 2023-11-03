95f7cf5fba5016257c9581ba355958e6a3be9723
NyuB <brice.decaestecker@gmx.fr>
2023-11-03 14:02:16 +0100

    Reorganized tests

--- tests/Main_test.elm
+++ tests/Main_test.elm
@@ -6,6 +6,61 @@ import Test exposing (..)
 import Visibility exposing (..)
 
 
+suite : Test
+suite =
+    describe "Main logic"
+        [ visibility_suite
+        ]
+
+
+visibility_suite : Test
+visibility_suite =
+    describe "Visibility"
+        [ test "Visibility at init None" <|
+            \_ -> Expect.equalLists [ False, False, False, False ] (visibility [ init_none.expected, init_none.actual, init_none.diff ] ++ visibility [ init_none.image ])
+        , test "Visibility at init with flags" <|
+            \_ -> Expect.equalLists [ True, True, True, True ] (visibility [ init_some.expected, init_some.actual, init_some.diff ] ++ visibility [ init_some.image ])
+        , test "Toggle expected visibility" <|
+            \_ ->
+                let
+                    ( updated, _ ) =
+                        update (ToggleContour Expected) init_some
+                in
+                Expect.equal updated.expected (Hidden (content init_some.expected))
+        , test "Toggle actual visibility" <|
+            \_ ->
+                let
+                    ( updated, _ ) =
+                        update (ToggleContour Actual) init_some
+                in
+                Expect.equal updated.actual (Hidden (content init_some.actual))
+        , test "Toggle diff visibility" <|
+            \_ ->
+                let
+                    ( updated, _ ) =
+                        update (ToggleContour Diff) init_some
+                in
+                Expect.equal updated.diff (Hidden (content init_some.diff))
+        , test "Toggle image visibility" <|
+            \_ ->
+                let
+                    ( updated, _ ) =
+                        update ToggleImage init_some
+                in
+                Expect.equal updated.image (Hidden (content init_some.image))
+        , test "Updating image url makes it visible" <|
+            \_ ->
+                let
+                    ( updated, _ ) =
+                        update (ChangeImageUrl "test") init_none
+                in
+                Expect.equal True (isVisible updated.image)
+        , test "If init flags are passed, components are visible from start" <|
+            \_ ->
+                Expect.equal True (isVisible init_some.image)
+        ]
+
+
 visibility : List (Visibility a) -> List Bool
 visibility l =
     List.map isVisible l
@@ -20,10 +75,6 @@ init_none =
     m
 
 
-test_flags =
-    { image = { url = "test.jpg", width = 10, height = 20, refX = 0.0, refY = 0.0, pixelWidth = 1.0, pixelHeight = 1.0 }, expected = [ [] ], actual = [ [] ], diff = [ [] ] }
-
-
 init_some : Main.Model
 init_some =
     let
@@ -33,51 +84,6 @@ init_some =
     m
 
 
-suite : Test
-suite =
-    describe "Main logic"
-        [ describe "Visibility"
-            [ test "Visibility at init None" <|
-                \_ -> Expect.equalLists [ False, False, False, False ] (visibility [ init_none.expected, init_none.actual, init_none.diff ] ++ visibility [ init_none.image ])
-            , test "Visibility at init with flags" <|
-                \_ -> Expect.equalLists [ True, True, True, True ] (visibility [ init_some.expected, init_some.actual, init_some.diff ] ++ visibility [ init_some.image ])
-            , test "Toggle expected visibility" <|
-                \_ ->
-                    let
-                        ( updated, _ ) =
-                            update (ToggleContour Expected) init_some
-                    in
-                    Expect.equal updated.expected (Hidden (content init_some.expected))
-            , test "Toggle actual visibility" <|
-                \_ ->
-                    let
-                        ( updated, _ ) =
-                            update (ToggleContour Actual) init_some
-                    in
-                    Expect.equal updated.actual (Hidden (content init_some.actual))
-            , test "Toggle diff visibility" <|
-                \_ ->
-                    let
-                        ( updated, _ ) =
-                            update (ToggleContour Diff) init_some
-                    in
-                    Expect.equal updated.diff (Hidden (content init_some.diff))
-            , test "Toggle image visibility" <|
-                \_ ->
-                    let
-                        ( updated, _ ) =
-                            update ToggleImage init_some
-                    in
-                    Expect.equal updated.image (Hidden (content init_some.image))
-            , test "Updating image url makes it visible" <|
-                \_ ->
-                    let
-                        ( updated, _ ) =
-                            update (ChangeImageUrl "test") init_none
-                    in
-                    Expect.equal True (isVisible updated.image)
-            , test "If init flags are passed, components are visible from start" <|
-                \_ ->
-                    Expect.equal True (isVisible init_some.image)
-            ]
-        ]
+test_flags : { image : { url : String, width : number, height : number, refX : Float, refY : Float, pixelWidth : Float, pixelHeight : Float }, expected : List (List a), actual : List (List b), diff : List (List c) }
+test_flags =
+    { image = { url = "test.jpg", width = 10, height = 20, refX = 0.0, refY = 0.0, pixelWidth = 1.0, pixelHeight = 1.0 }, expected = [ [] ], actual = [ [] ], diff = [ [] ] }
